e2b0e688bcf2b04b21d1613f3735d6b2e22e8e0a
Albena Kostova <albena.kostov@gmail.com>
2021-12-31 09:51:15 +0100

    .

---
+++ src/views/editor/editor.js
@@ -0,0 +1,30 @@
+import { html, render } from "../../lib.js";
+
+
+const template = () => html`
+<section id="editor" style="background: #ccc;">
+    <h2>Editor</h2> 
+    <div className="formContainer"  style="background: #red;">
+        <form className="form">
+            <label class="editor-label layout">
+                <span class="label">Title:</span>
+                <input type="text" name="title" class="input">
+            </label>
+            <label class="editor-label layout">
+                <span class="label">Category:</span>
+                <select type="text" name="category" class="input">
+                    <option value="choose">Choose category</option>
+                    <option value="mosaic">Mosaic</option>
+                    <option value="painting">Painting</option>
+                    <option value="other">Other</option>
+                </select>
+            </label>
+            <input type="submit" class="input" value="Save">
+        </form>
+    </div>    
+</section>
+`;
+
+export async function editorPage(ctx) {
+    ctx.render(template());
+}
